988b28eef9c5105b5c555af625b42d2dbca9480e
elastre <engells.lastre@gmail.com>
2018-08-15 23:33:33 -0500

    Login y Token

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.use(bodyParser.json());
 //Importamos Rutas
 var appRoutes = require("./routes/app");
 var usuarioRoutes = require("./routes/usuario");
+var loginRoutes = require("./routes/login");
 
 //Conexion a la base de datos
 mongoose.connection.openUri('mongodb://localhost:27017/trazableDB', (err,res)=>{
@@ -23,6 +24,7 @@ mongoose.connection.openUri('mongodb://localhost:27017/trazableDB', (err,res)=>{
 
 //Rutas 
 app.use('/usuario',usuarioRoutes);
+app.use('/login',loginRoutes);
 app.use('/',appRoutes);
 
 
